27f397434cb29909f288a393cdf7506d58603806
JP Kabler <john.kabler@gmail.com>
2016-01-03 10:49:56 -0600

    pandas integration test

--- first_app.py
+++ first_app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
+import pandas.io.data as web
+import datetime
 
 app = Flask(__name__)
 
@@ -6,6 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'This is the home page.  It does nothing.'
+
+@app.route('/pandas_test')
+def pandas_test():
+    start = datetime.datetime(2008, 5, 1)
+    end = datetime.datetime(2014, 8, 1)
+    f = web.DataReader('F', 'google', start, end)
+    return render_template('pandas.html', data=f.to_html())
+    #return f.to_html()
 
 if __name__ == '__main__':
     app.run(debug=True)
